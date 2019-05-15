6c5b31252fc6a48c5d71ae773ab0c1fdc23fd6e9
neo <hao@19931007>
2019-05-15 10:46:05 +0800

    modbus暂停

---
+++ module-modbus/src/main/java/com/springboot/module/ModbusApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModbusApplication.class, args);
+	}
+
+}
