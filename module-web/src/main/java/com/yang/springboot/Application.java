454085c85154ff8510fe6869cde67ddb3fe38476
neo <hao@19931007>
2019-05-16 11:05:23 +0800

    更新记录

--- module-web/src/main/java/com/yang/springboot/Application.java
+++ module-web/src/main/java/com/yang/springboot/Application.java
@@ -1,5 +1,6 @@
 package com.yang.springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,9 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author yanghao
  * @date 2019-04-24 15:32
  */
+@Slf4j
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        log.info("启动成功1.。。。");
     }
 }
