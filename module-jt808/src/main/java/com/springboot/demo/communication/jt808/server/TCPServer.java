0f03bf5ba83a4edf5ac3b3d534e7caf1afb20825
YangHao <yangh@acloudchina.com>
2019-10-21 15:10:17 +0800

    注释

--- module-jt808/src/main/java/com/springboot/demo/communication/jt808/server/TCPServer.java
+++ module-jt808/src/main/java/com/springboot/demo/communication/jt808/server/TCPServer.java
@@ -43,20 +43,21 @@ public class TCPServer {
         this.bossGroup = new NioEventLoopGroup();
         this.workerGroup = new NioEventLoopGroup();
         ServerBootstrap serverBootstrap = new ServerBootstrap();
-        serverBootstrap.group(bossGroup, workerGroup)
-                .channel(NioServerSocketChannel.class)
-                .childHandler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    public void initChannel(SocketChannel ch) throws Exception {
-                        ch.pipeline().addLast("idleStateHandler", new IdleStateHandler(30, 0, 0, TimeUnit.MINUTES));
-                        ch.pipeline().addLast(new Decoder4LoggingOnly());
+        serverBootstrap.group(bossGroup, workerGroup);
+        serverBootstrap.channel(NioServerSocketChannel.class);
+        //childHandler()用于指定处理新连接数据的读写处理逻辑
+        serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            public void initChannel(SocketChannel ch) throws Exception {
+                ch.pipeline().addLast("idleStateHandler", new IdleStateHandler(30, 0, 0, TimeUnit.MINUTES));
+                ch.pipeline().addLast(new Decoder4LoggingOnly());
 
-                        //1024表示单条消息的最大长度，解码器在查找分隔符的时候，达到该长度还没找到的话会抛异常
-                        ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, Unpooled.copiedBuffer(new byte[]{0x7e}), Unpooled.copiedBuffer(new byte[]{0x7e, 0x7e})));
-                        ch.pipeline().addLast(new TCPServerHandler());
-                    }
-                }).option(ChannelOption.SO_BACKLOG, 128)
-                .childOption(ChannelOption.SO_KEEPALIVE, true);
+                //1024表示单条消息的最大长度，解码器在查找分隔符的时候，达到该长度还没找到的话会抛异常
+                ch.pipeline().addLast(new DelimiterBasedFrameDecoder(1024, Unpooled.copiedBuffer(new byte[]{0x7e}), Unpooled.copiedBuffer(new byte[]{0x7e, 0x7e})));
+                ch.pipeline().addLast(new TCPServerHandler());
+            }
+        }).option(ChannelOption.SO_BACKLOG, 128)//表示系统用于临时存放已完成三次握手的请求的队列的最大长度，如果连接建立频繁，服务器处理创建新连接较慢，可以适当调大这个参数
+                .childOption(ChannelOption.SO_KEEPALIVE, true);//ChannelOption.SO_KEEPALIVE表示是否开启TCP底层心跳机制，true为开启
 
         this.log.info("TCP服务启动完毕,port={}", this.port);
         ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
